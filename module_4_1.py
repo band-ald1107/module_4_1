8b7287a8791112c85bbdc43ee837ed6a6df3b003
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-10-09 20:45:22 +0800

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fd1
+from true_math import divide as td2
+
+print (fd1(69,3))
+print (fd1(3,0))
+print (td2(49,7))
+print (td2(15,0))
---
+++ true_math.py
@@ -0,0 +1,8 @@
+import math
+
+
+def divide(first, second):
+    if second == 0:
+       print(-math.inf)
+    else:
+        return first / second
